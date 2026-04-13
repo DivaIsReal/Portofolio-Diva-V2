c2809a7a3bf968d47ed1254609e4f066d4f73672
DivaIsReal <divaahmadpradana72@gmail.com>
2026-04-13 18:53:29 +0700

    Configure Sanity studioHost for non-interactive deploys

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -2,6 +2,7 @@ const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'your_project_id';
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production';
 
 export default {
+  studioHost: 'divaisreal',
   api: {
     projectId,
     dataset,
